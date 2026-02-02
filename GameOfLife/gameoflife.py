fe9f00df7800552756e3a595b4dc13db09d44668
Jakub Konior <koniorjk@gmail.com>
2026-02-02 22:51:49 +0100

    add random generation and changed var names

--- GameOfLife/gameoflife.py
+++ GameOfLife/gameoflife.py
@@ -1,34 +1,27 @@
-import time, copy, os
+import time, copy, random
 
-WIDTH = 6
-HEIGHT = 6
-SIZE = 6
-boarda = []
+WIDTH = 20
+HEIGHT = 20
+SIZE = 20
+board = []
 
 # Generowanie planszy
 for x in range(WIDTH):
     for y in range(HEIGHT):
-        boarda.append(0)
+        board.append(random.randint(0,1))
 
-boarda = [boarda[i:i+SIZE] for i in range(0, len(boarda), SIZE)]
+board = [board[i:i+SIZE] for i in range(0, len(board), SIZE)]
 
-boarda[2][3] = 1
-boarda[3][4] = 1
-boarda[4][4] = 1
-boarda[4][3] = 1
-boarda[4][2] = 1
-
+# Pętla główna
 while True:
-    board = copy.deepcopy(boarda)
+    currentBoard = copy.deepcopy(board)
     # rysowanie planszy
     for x in range(WIDTH):
         for y in range(HEIGHT):
-                print(str(boarda[x][y]) + ' ',end="")
+                print(str(board[x][y]) + ' ',end="")
         print()
 
-
     # sprawdzanie pól sąsiednich
-
     for x in range(WIDTH):
         for y in range(HEIGHT):
             left = (x-1)%WIDTH
@@ -37,27 +30,28 @@ while True:
             down = (y+1)%HEIGHT
 
             numOfNeighbours = 0
-            if board[left][up] == 1:
+            if currentBoard[left][up] == 1:
                 numOfNeighbours += 1
-            if board[left][y] == 1:
+            if currentBoard[left][y] == 1:
                 numOfNeighbours += 1
-            if board[left][down] == 1:
+            if currentBoard[left][down] == 1:
                 numOfNeighbours += 1
-            if board[right][up] == 1:
+            if currentBoard[right][up] == 1:
                 numOfNeighbours += 1
-            if board[right][y] == 1:
+            if currentBoard[right][y] == 1:
                 numOfNeighbours += 1
-            if board[right][down] == 1:
+            if currentBoard[right][down] == 1:
                 numOfNeighbours += 1
-            if board[x][up] == 1:
+            if currentBoard[x][up] == 1:
                 numOfNeighbours += 1
-            if board[x][down] == 1:
+            if currentBoard[x][down] == 1:
                 numOfNeighbours += 1
 
-            if (numOfNeighbours == 3 or numOfNeighbours == 2) and board[x][y] == 1:
-                boarda[x][y] = 1
-            elif numOfNeighbours == 3 and board[x][y] == 0:
-                boarda[x][y] = 1
+            # logika gry
+            if (numOfNeighbours == 3 or numOfNeighbours == 2) and currentBoard[x][y] == 1:
+                board[x][y] = 1
+            elif numOfNeighbours == 3 and currentBoard[x][y] == 0:
+                board[x][y] = 1
             else:
-                boarda[x][y] = 0
+                board[x][y] = 0
     time.sleep(1)
